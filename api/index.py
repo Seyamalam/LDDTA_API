5e77a24c23caf5d75967a763ba6c1ef425781c9a
Seyamalam <seyamalam41@gmail.com>
2024-03-21 01:08:44 +0600

    Tried Turning the exisiting api into a vercel server function.

--- api/index.py
+++ api/index.py
@@ -1,28 +1,19 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import request, jsonify
 import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
-
-app = Flask(__name__)
-CORS(app)
 
 model_path = "tomato.h5"
 MODEL = tf.keras.models.load_model(model_path)
 
 CLASS_NAMES = ["Tomato___Bacterial_spot", "Tomato___Early_blight", "Tomato___Late_blight", "Tomato___Leaf_Mold", "Tomato___Septoria_leaf_spot", "Tomato___Spider_mites Two-spotted_spider_mite", "Tomato___Target_Spot", "Tomato___Tomato_Yellow_Leaf_Curl_Virus","Tomato___Tomato_mosaic_virus", "Tomato___healthy"]
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "Hello, I am alive"
-
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
-@app.route('/predict', methods=['POST'])
-def predict():
+def handler(request):
     file = request.files['file']
     image = read_file_as_image(file.read())
     img_batch = np.expand_dims(image, 0)
@@ -35,6 +26,3 @@ def predict():
         'class': predicted_class,
         'confidence': float(confidence)
     })
-
-if __name__ == "__main__":
-    app.run(host='localhost', port=8000)
